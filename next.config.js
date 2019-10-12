50e532e18678085e3d0fd6fdd393eb9c380b95bf
nezouse <stankiewicz1998@gmail.com>
2019-10-12 21:54:27 +0200

    Disable css purging in dev builds

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
 
-module.exports = withCss(withPurgeCss());
+module.exports = withCss(
+  withPurgeCss({
+    purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer
+  })
+);
