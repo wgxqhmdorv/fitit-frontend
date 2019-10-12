74ea06115cf6c094e3de2f01026d8ec14ebde39c
nezouse <stankiewicz1998@gmail.com>
2019-10-12 10:45:26 +0200

    Revert PurgeCSS config changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,10 @@
-const purgecss = require("@fullhuman/postcss-purgecss");
-
-const plugins = [
-  require("tailwindcss"),
-  require("postcss-preset-env"),
-  require("autoprefixer"),
-  require("cssnano")({
-    preset: "default"
-  })
-];
-
-if (process.env.NODE_ENV === "production") {
-  plugins.push(
-    purgecss({
-      content: ["./components/**/*.js", "./pages/**/*.js"],
-      whitelist: ["html", "body"]
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("postcss-preset-env"),
+    require("autoprefixer"),
+    require("cssnano")({
+      preset: "default"
     })
-  );
-}
-
-module.exports = { plugins };
+  ]
+};
