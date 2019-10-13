25d4f5a1294ea00e539856ac357097efb5b564b8
nezouse <stankiewicz1998@gmail.com>
2019-10-13 23:18:54 +0200

    Change purgeCss config to work with tailwind classes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 const withCss = require("@zeit/next-css");
-const withPurgeCss = require("next-purgecss");
 
-module.exports = withCss(
-  withPurgeCss({
-    purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer
-  })
-);
+module.exports = withCss();
