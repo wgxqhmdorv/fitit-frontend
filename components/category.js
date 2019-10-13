a07ce85e224651c7dffdbd583598409051e4d5e4
nezouse <stankiewicz1998@gmail.com>
2019-10-14 00:02:29 +0200

    Use function to calculate macros summary

--- components/category.js
+++ components/category.js
@@ -1,38 +1,17 @@
+const reduceObjects = (array, key) =>
+  array.reduce((prevValue, nextValue) => prevValue + nextValue[key], 0);
+
 const Category = ({ items }) => {
   return (
     <div className="mb-2">
       <p className="font-bold">Summary</p>
       <div className="text-xs text-temp ml-2">
         <div className="flex">
-          <p>
-            {items.reduce(
-              (prevValue, nextValue) => prevValue + nextValue.calories,
-              0
-            )}{" "}
-            kcal
-          </p>
+          <p>{reduceObjects(items, "calories")} kcal</p>
           <div className="flex w-1/2 justify-around ml-8">
-            <p>
-              {items.reduce(
-                (prevValue, nextValue) => prevValue + nextValue.carbohydrates,
-                0
-              )}{" "}
-              g
-            </p>
-            <p>
-              {items.reduce(
-                (prevValue, nextValue) => prevValue + nextValue.proteins,
-                0
-              )}{" "}
-              g
-            </p>
-            <p>
-              {items.reduce(
-                (prevValue, nextValue) => prevValue + nextValue.fats,
-                0
-              )}{" "}
-              g
-            </p>
+            <p>{reduceObjects(items, "carbohydrates")} g</p>
+            <p>{reduceObjects(items, "proteins")} g</p>
+            <p>{reduceObjects(items, "fats")} g</p>
           </div>
         </div>
       </div>
