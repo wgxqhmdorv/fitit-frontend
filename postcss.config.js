4e9639f44a5d19c4a3031d60de32534a061b3d13
nezouse <stankiewicz1998@gmail.com>
2019-10-13 23:18:54 +0200

    Change purgeCss config to work with tailwind classes

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,15 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./components/*.js", "./pages/*.js"],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   plugins: [
     require("tailwindcss"),
     require("postcss-preset-env"),
     require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
     require("cssnano")({
       preset: "default"
     })
